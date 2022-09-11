a057be89ffee0fa5374b6adcd4c5b138e45c5cd8
kierenwuest <112849006+kierenwuest@users.noreply.github.com>
2022-09-11 09:58:38 +0930

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+//------------------ Greatest Archer In The Land ------------------//
+// A ambitious RPG Battle simultor by Kieren Wuest Sept 2022
+// Ambitious as this is likely well over my expereince level andnot based on a tutorial
+// I may need to come back to this later
 
+// const palette = { 
+// 	// https://coolors.co/palette/8ecae6-219ebc-023047-ffb703-fb8500
+// 	"Light Cornflower Blue":"#8ecae6",
+// 	"Blue Green":"#219ebc",
+// 	"Prussian Blue":"#023047",
+// 	"Selective Yellow":"#ffb703",
+// 	"Tangerine":"#fb8500"
+// }
+
+// 	"Light Cornflower Blue": rgba(142, 202, 230, 1);
+// 	"Blue Green": rgba(33, 158, 188, 1);
+// 	"Prussian Blue": rgba(2, 48, 71, 1);
+// 	"Selective Yellow": rgba(255, 183, 3, 1);
+// 	"Tangerine": rgba(251, 133, 0, 1);
+
+// Find most efficent for 4 canvases
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+
+// const W = (canvas.width = 1200);  //Not ness
+// const H = (canvas.height = 600);  //Not ness
+
+
+//Not ness
+// document.getElementById('gameDiv').setAttribute("style","width:"+ W +"px;height:"+ H +"px"); //eergh
+// document.getElementById('playerDiv').setAttribute("style","background: #ffb703; left: "+ 110 +"px");
+// document.getElementById('enemyDiv').setAttribute("style","background: #ffb903; left: "+ ((W/3)*2 + 110 ) +"px");
